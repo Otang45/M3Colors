2a93a07827a216d18c8e81fec3b17d4b197dcb2b
Otang45 <dikiotang@gmail.com>
2024-11-29 16:13:49 +0700

    shrink and minify

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
+        multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -23,7 +24,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
